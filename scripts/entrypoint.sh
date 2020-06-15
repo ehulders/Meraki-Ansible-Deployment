67085bcd8ec1c3f3b1f46a8fc42ff578cfcf8226
Stuart Traynor <sttrayno@cisco.com>
2020-06-15 18:12:28 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -11,6 +11,7 @@ for dir in */; do
    cp -r $variable $dir
    ls dir
    cp ../../deploy-branch-readyaml.yaml $dir/deploy-branch-readyaml.yaml
-   ansible-playbook deploy-branch-readyaml.yaml
+   ansible-playbook deploy-branch-readyaml.yaml --extra-vars "auth=$auth org=$demo" -vvvv
    cd ../..
 done
+ 

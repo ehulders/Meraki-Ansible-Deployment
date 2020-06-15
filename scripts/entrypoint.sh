71701e46c370a01a8aa8e72ff854b0aa21d5a256
Stuart Traynor <sttrayno@cisco.com>
2020-06-15 18:30:58 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,11 +7,12 @@ for dir in */; do
    cd "$dir"
    variable=$(pwd)
    echo $variable
-   mkdir $dir
-   cp -r $variable $dir
-   ls dir
-   cp ../../deploy-branch-readyaml.yaml $dir/deploy-branch-readyaml.yaml
-   ansible-playbook deploy-branch-readyaml.yaml --extra-vars "auth=$auth org=$demo" -vvvv
+   mkdir deploy
+   cp -r $variable/addressing.yaml deploy/addressing.yaml
+   cp -r $variable/devices.yaml deploy/devices.yaml
+   ls deploy
+   cp ../../deploy-branch-readyaml.yaml deploy/deploy-branch-readyaml.yaml
+   ansible-playbook ./deploy/deploy-branch-readyaml.yaml --extra-vars "auth=$auth org=$demo" -vvvv
    cd ../..
 done
  

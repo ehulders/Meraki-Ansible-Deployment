4311d06d81a740d21db129f6a405a2d7f0841f1e
Eric Hulderson <ehulders@cisco.com>
2022-02-10 10:20:11 -0800

    switching back to deploy-branch-readyaml

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,13 +6,13 @@ for dir in */; do
    cd "$dir"
    path=$(pwd)
    mkdir deploy
-   #cp -r $path/addresses.yaml deploy/addresses.yaml
-   #cp -r $path/devices.yaml deploy/devices.yaml
-   cp -r $path/switches.yaml deploy/switches.yaml
+   cp -r $path/addresses.yaml deploy/addresses.yaml
+   cp -r $path/devices.yaml deploy/devices.yaml
+   #cp -r $path/switches.yaml deploy/switches.yaml
    ls deploy
-   #cp ../../deploy-branch-readyaml.yaml deploy/deploy-branch-readyaml.yaml
-   #ansible-playbook ./deploy/deploy-branch-readyaml.yaml --extra-vars "auth=$auth org='Cisco Systems, Inc.'"
-   cp ../../update-switch.yaml deploy/update-switch.yaml
-   ansible-playbook ./deploy/update-switch.yaml --extra-vars "auth=$auth org='Cisco Systems, Inc.'"
+   cp ../../deploy-branch-readyaml.yaml deploy/deploy-branch-readyaml.yaml
+   ansible-playbook ./deploy/deploy-branch-readyaml.yaml --extra-vars "auth=$auth org='Cisco Systems, Inc.'"
+   #cp ../../update-switch.yaml deploy/update-switch.yaml
+   #ansible-playbook ./deploy/update-switch.yaml --extra-vars "auth=$auth org='Cisco Systems, Inc.'"
    cd ..
 done
